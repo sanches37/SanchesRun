a944b0fb9d0be0bc3dcb36b83285cd9f23bba5de
sanches37 <xognsl87@gmail.com>
2023-02-27 15:59:44 +0900

    Initial Commit

---
+++ SanchesRun/SanchesRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanchesRunApp.swift
+//  SanchesRun
+//
+//  Created by tae hoon park on 2023/02/27.
+//
+
+import SwiftUI
+
+@main
+struct SanchesRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
