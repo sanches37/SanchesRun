4f6dcb5c5ff9987ea1dfcbaf292df7cf083962ed
sanches37 <xognsl87@gmail.com>
2023-03-10 15:56:32 +0900

    CloudKit을 사용하도록 코드 추가

--- SanchesRun/CoreData/PersistenceController.swift
+++ SanchesRun/CoreData/PersistenceController.swift
@@ -9,13 +9,19 @@ import CoreData
 
 struct PersistenceController {
   static let shared = PersistenceController()
-  let container: NSPersistentContainer
+  let container: NSPersistentCloudKitContainer
   var viewContext: NSManagedObjectContext {
     container.viewContext
   }
   
   private init() {
-    container = NSPersistentContainer(name: "SanchesRun")
+    container = NSPersistentCloudKitContainer(name: "SanchesRun")
+    guard let description = container.persistentStoreDescriptions.first else {
+      fatalError("Failed to initialize persistent container")
+    }
+    description.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
+    container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+    container.viewContext.automaticallyMergesChangesFromParent = true
     container.loadPersistentStores { storeDescription, error in
       if let nserror = error as NSError? {
         fatalError("Unresolved error: \(nserror), \(nserror.userInfo)")
